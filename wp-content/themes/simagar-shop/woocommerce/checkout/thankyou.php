8f38f9cc5c2ca9c1b8ec3d096c29816682c2c1a9
sobhan zarin <sobhan.zarrinpour82@gmail.com>
2026-01-07 00:03:36 +0330

    already push 1/7/2026

--- wp-content/themes/simagar-shop/woocommerce/checkout/thankyou.php
+++ wp-content/themes/simagar-shop/woocommerce/checkout/thankyou.php
@@ -10,26 +10,23 @@
  * happen. When this occurs the version of the template file will be bumped and
  * the readme will list any important changes.
  *
- * @see     https://docs.woocommerce.com/document/template-structure/
- * @package WooCommerce/Templates
+ * @see https://woocommerce.com/document/template-structure/
+ * @package WooCommerce\Templates
  * @version 8.1.0
+ *
+ * @var WC_Order $order
  */
 
 defined( 'ABSPATH' ) || exit;
-
-$wrapper_classes = '';
-
-if ( woodmart_get_opt( 'thank_you_page_extra_content' ) ) {
-	$wrapper_classes .= ' wd-with-extra-content';
-}
-
 ?>
 
-<div class="woocommerce-order<?php echo esc_attr( $wrapper_classes ); ?>">
+<div class="woocommerce-order">
 
-	<?php if ( $order ) : ?>
+	<?php
+	if ( $order ) :
 
-		<?php do_action( 'woocommerce_before_thankyou', $order->get_id() ); ?>
+		do_action( 'woocommerce_before_thankyou', $order->get_id() );
+		?>
 
 		<?php if ( $order->has_status( 'failed' ) ) : ?>
 
@@ -43,30 +40,46 @@ if ( woodmart_get_opt( 'thank_you_page_extra_content' ) ) {
 			</p>
 
 		<?php else : ?>
-			<?php if ( woodmart_get_opt( 'thank_you_page_extra_content' ) || woodmart_get_opt( 'thank_you_page_html_block' ) ) : ?>
-				<div class="wd-order-extra-content wd-entry-content">
-					<?php if ( 'text' === woodmart_get_opt( 'thank_you_page_content_type', 'text' ) ) : ?>
-						<?php echo do_shortcode( woodmart_get_opt( 'thank_you_page_extra_content' ) ); ?>
-					<?php else : ?>
-						<?php echo woodmart_get_html_block( woodmart_get_opt( 'thank_you_page_html_block' ) ); ?>
-					<?php endif; ?>
-				</div>
-			<?php endif; ?>
 
-			<?php if ( woodmart_get_opt( 'thank_you_page_default_content' ) ) : ?>
+			<?php wc_get_template( 'checkout/order-received.php', array( 'order' => $order ) ); ?>
 
-				<?php wc_get_template( 'checkout/order-received.php', array( 'order' => $order ) ); ?>
+			<ul class="woocommerce-order-overview woocommerce-thankyou-order-details order_details">
 
-				<?php woodmart_order_overview( $order ); ?>
-		
-			<?php endif; ?>
+				<li class="woocommerce-order-overview__order order">
+					<?php esc_html_e( 'Order number:', 'woocommerce' ); ?>
+					<strong><?php echo $order->get_order_number(); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></strong>
+				</li>
+
+				<li class="woocommerce-order-overview__date date">
+					<?php esc_html_e( 'Date:', 'woocommerce' ); ?>
+					<strong><?php echo wc_format_datetime( $order->get_date_created() ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></strong>
+				</li>
+
+				<?php if ( is_user_logged_in() && $order->get_user_id() === get_current_user_id() && $order->get_billing_email() ) : ?>
+					<li class="woocommerce-order-overview__email email">
+						<?php esc_html_e( 'Email:', 'woocommerce' ); ?>
+						<strong><?php echo $order->get_billing_email(); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></strong>
+					</li>
+				<?php endif; ?>
+
+				<li class="woocommerce-order-overview__total total">
+					<?php esc_html_e( 'Total:', 'woocommerce' ); ?>
+					<strong><?php echo $order->get_formatted_order_total(); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></strong>
+				</li>
+
+				<?php if ( $order->get_payment_method_title() ) : ?>
+					<li class="woocommerce-order-overview__payment-method method">
+						<?php esc_html_e( 'Payment method:', 'woocommerce' ); ?>
+						<strong><?php echo wp_kses_post( $order->get_payment_method_title() ); ?></strong>
+					</li>
+				<?php endif; ?>
+
+			</ul>
 
 		<?php endif; ?>
 
-		<?php if ( woodmart_get_opt( 'thank_you_page_default_content' ) ) : ?>
-			<?php do_action( 'woocommerce_thankyou_' . $order->get_payment_method(), $order->get_id() ); ?>
-			<?php do_action( 'woocommerce_thankyou', $order->get_id() ); ?>
-		<?php endif; ?>
+		<?php do_action( 'woocommerce_thankyou_' . $order->get_payment_method(), $order->get_id() ); ?>
+		<?php do_action( 'woocommerce_thankyou', $order->get_id() ); ?>
 
 	<?php else : ?>
 
